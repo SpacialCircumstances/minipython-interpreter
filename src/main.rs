25b5f399941550921bbc9cb24a9f38a3c02e9368
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-29 22:39:47 +0100

    Parsers for simple expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use pom::parser::*;
+use pom::char_class::alphanum;
+use crate::Ast::*;
+
 #[derive(Debug, Eq, PartialEq)]
 enum Ast<'a> {
     Def { name: &'a str, parameters: Vec<&'a str>, body: Vec<Ast<'a>> },
@@ -13,6 +17,22 @@ struct Program<'a> {
     input: Vec<&'a str>,
     output: Vec<&'a str>,
     body: Vec<Ast<'a>>
+}
+
+fn var_name() -> Parser<u8, &str> {
+    is_a(alphanum).repeat(0..).map(|chars| chars.concat())
+}
+
+fn return_expr() -> Parser<u8, Ast> {
+    (seq(b"return ") * var_name()).map(|name| Return { name })
+}
+
+fn incr() -> Parser<u8, Ast> {
+    (var_name() - seq(b"+=1".as_ref())).map(|var_name| Incr { var_name })
+}
+
+fn decr() -> Parser<u8, Ast> {
+    (var_name() - seq(b"-=1".as_ref())).map(|var_name| Decr { var_name })
 }
 
 fn main() {
