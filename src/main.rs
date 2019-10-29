d5327cfc5db85dc93bd214243af93f4be5303fcb
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-29 21:27:23 +0100

    Create AST for minipython

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+enum Ast<'a> {
+    Def { name: &'a str, parameters: Vec<&'a str>, body: Vec<Ast<'a>> },
+    Return { name: &'a str },
+    While { cond_var: &'a str, body: Vec<Ast<'a>> },
+    Assign { var_name: &'a str, fun_name: &'a str, args: Vec<&'a str> },
+    Incr { var_name: &'a str },
+    Decr { var_name: &'a str }
+}
+
 fn main() {
     println!("Hello, world!");
 }
