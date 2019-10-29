c1e0b3ba6fea575e16a6542b6043b1f8aaba597f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-29 22:27:02 +0100

    Add type for program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Eq, PartialEq)]
 enum Ast<'a> {
     Def { name: &'a str, parameters: Vec<&'a str>, body: Vec<Ast<'a>> },
     Return { name: &'a str },
@@ -5,6 +6,13 @@ enum Ast<'a> {
     Assign { var_name: &'a str, fun_name: &'a str, args: Vec<&'a str> },
     Incr { var_name: &'a str },
     Decr { var_name: &'a str }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Program<'a> {
+    input: Vec<&'a str>,
+    output: Vec<&'a str>,
+    body: Vec<Ast<'a>>
 }
 
 fn main() {
