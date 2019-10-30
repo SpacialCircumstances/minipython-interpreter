8a536d4333ed338f23942ff976fcbbc3e4dab283
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-30 09:25:12 +0100

    Add parsers for while and def

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use pom::parser::*;
 use crate::Ast::*;
 use pom::set::Set;
-use std::iter::Peekable;
 
 #[derive(Debug, Eq, PartialEq)]
 enum Ast<'a> {
@@ -18,6 +17,10 @@ struct Program<'a> {
     input: Vec<&'a str>,
     output: Vec<&'a str>,
     body: Vec<Ast<'a>>
+}
+
+fn whitespace<'a>() -> Parser<'a, char, ()> {
+    is_a(|c: char| c.is_whitespace()).repeat(0..).discard()
 }
 
 fn separator<'a>() -> Parser<'a, char, ()> {
@@ -40,17 +43,39 @@ fn decr<'a>() -> Parser<'a, char, Ast<'a>> {
     (var_name() - tag("-=1")).map(|var_name| Decr { var_name: &var_name })
 }
 
+fn args<'a>() -> Parser<'a, char, Vec<&'a str>> {
+    sym('(') * list(var_name(), sym(',')) - sym(')')
+}
+
 fn assign<'a>() -> Parser<'a, char, Ast<'a>> {
-    let args = sym(')') * list(var_name(), sym(',')) - sym(')');
-    (var_name() - tag("=") + var_name() + args).map(|((v, f), arguments)| Assign {
+    (var_name() - tag("=") + var_name() + args()).map(|((v, f), arguments)| Assign {
         var_name: v,
         fun_name: f,
         args: arguments
     })
 }
 
+fn while_expr<'a>() -> Parser<'a, char, Ast<'a>> {
+    let head = tag("while ") * var_name() - tag("!=0:") - whitespace();
+    let body = list(call(expression), separator());
+    (head + body - tag("#endwhile")).map(|(name, body)| While {
+        cond_var: name,
+        body: body
+    })
+}
+
+fn def_expr<'a>() -> Parser<'a, char, Ast<'a>> {
+    let head = tag("def ") * var_name() + args() - tag(":");
+    let body = list(call(expression), separator());
+    (head + body - tag("#enddef")).map(|((fname, fargs), body)| Def {
+        name: fname,
+        parameters: fargs,
+        body
+    })
+}
+
 fn expression<'a>() -> Parser<'a, char, Ast<'a>> {
-    incr() | decr() | return_expr() | assign()
+    incr() | decr() | return_expr() | assign() | while_expr() | def_expr()
 }
 
 fn main() {
