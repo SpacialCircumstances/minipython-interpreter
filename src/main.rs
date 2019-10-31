1b2c3154015170de9221a0f3435b3778bb4e72d8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-31 01:15:46 +0100

    Implement running interactive programs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod interpreter;
 use std::fs;
 use indexmap::map::IndexMap;
 use clap::{App, Arg};
+use std::io::stdin;
 
 fn main() {
     let matches = App::new("minipython")
@@ -24,14 +25,35 @@ fn main() {
     if let Some(filename) = matches.value_of("file") {
         let code_bytes = fs::read(filename).unwrap();
         let code: Vec<char> = String::from_utf8(code_bytes).unwrap().chars().collect();
-        let program_parser = parser::program();
-        match program_parser.parse(&code) {
-            Ok(ast) => {
-                let env_initial = IndexMap::new();
-                let mut env = interpreter::Env::new(&env_initial);
-                interpreter::interpret_program(&mut env, &ast);
-            },
-            Err(e) => println!("{}", e)
+        if matches.is_present("interactive") {
+            let program_parser = parser::interactive_program();
+            match program_parser.parse(&code) {
+                Ok(iprog) => {
+                    let mut env_initial: IndexMap<&str, i32> = IndexMap::new();
+                    for input in &iprog.inputs {
+                        println!("{}=", input);
+                        let mut str_value = String::new();
+                        stdin().read_line(&mut str_value).expect("Failed to read line");
+                        let value: i32 = str_value.trim().parse().expect("Failed to read number");
+                        env_initial.insert(input, value);
+                    }
+                    let mut env = interpreter::Env::new(&env_initial);
+                    env.set_result(&iprog.output);
+                    interpreter::interpret_program(&mut env, &iprog.program);
+                    println!("Result: {}={}", &iprog.output, env.get_result().unwrap());
+                },
+                Err(e) => println!("{}", e)
+            }
+        } else {
+            let program_parser = parser::program();
+            match program_parser.parse(&code) {
+                Ok(ast) => {
+                    let env_initial = IndexMap::new();
+                    let mut env = interpreter::Env::new(&env_initial);
+                    interpreter::interpret_program(&mut env, &ast);
+                },
+                Err(e) => println!("{}", e)
+            }
         }
     }
 }
